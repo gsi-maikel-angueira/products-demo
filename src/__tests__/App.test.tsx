4f86b5bc42c56635c9fec133c227c01d39687f20
mk3l <maikel.angueira@generalsoftwareinc.com>
2024-02-12 15:36:46 -0500

    add configuration to add Jest test on vite

---
+++ src/__tests__/App.test.tsx
@@ -0,0 +1,25 @@
+import "@testing-library/jest-dom";
+import once from "./once";
+
+test("demo", () => {
+  expect(true).toBe(true);
+});
+
+test("without once", () => {
+  const myFn = jest.fn();
+  myFn();
+  myFn();
+  myFn();
+
+  expect(myFn).toHaveBeenCalledTimes(3);
+});
+
+test("with 'once', a function runs one time", () => {
+  const myFn = jest.fn();
+  const onceFn = jest.fn(once(myFn));
+  onceFn();
+  onceFn();
+  onceFn();
+  expect(onceFn).toHaveBeenCalledTimes(3);
+  expect(myFn).toHaveBeenCalledTimes(1);
+});
---
+++ src/__tests__/once.ts
@@ -0,0 +1,11 @@
+const once = <FNType extends (...args: any[]) => any>(fn: FNType) => {
+  let done = false;
+  return ((...args: Parameters<FNType>) => {
+    if (!done) {
+      done = true;
+      return fn(...args);
+    }
+  }) as FNType;
+};
+
+export default once;
