9733ad3ca4a867a0e2d9fc02f9da49fab32c0235
mk3l <maikel.angueira@generalsoftwareinc.com>
2024-02-12 15:40:50 -0500

    Grouping on test suite

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,25 +1,27 @@
 import "@testing-library/jest-dom";
 import once from "./once";
 
-test("demo", () => {
-  expect(true).toBe(true);
-});
+describe("Test Once function", () => {
+  test("demo", () => {
+    expect(true).toBe(true);
+  });
 
-test("without once", () => {
-  const myFn = jest.fn();
-  myFn();
-  myFn();
-  myFn();
+  test("without once", () => {
+    const myFn = jest.fn();
+    myFn();
+    myFn();
+    myFn();
 
-  expect(myFn).toHaveBeenCalledTimes(3);
-});
+    expect(myFn).toHaveBeenCalledTimes(3);
+  });
 
-test("with 'once', a function runs one time", () => {
-  const myFn = jest.fn();
-  const onceFn = jest.fn(once(myFn));
-  onceFn();
-  onceFn();
-  onceFn();
-  expect(onceFn).toHaveBeenCalledTimes(3);
-  expect(myFn).toHaveBeenCalledTimes(1);
+  test("with 'once', a function runs one time", () => {
+    const myFn = jest.fn();
+    const onceFn = jest.fn(once(myFn));
+    onceFn();
+    onceFn();
+    onceFn();
+    expect(onceFn).toHaveBeenCalledTimes(3);
+    expect(myFn).toHaveBeenCalledTimes(1);
+  });
 });
